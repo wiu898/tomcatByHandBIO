a6776d1e1189d5edad7ccd06c578998fe38415ce
Apple0Juice <1033489566@qq.com>
2021-05-13 16:07:24 +0800

    手写tomcat

---
+++ src/main/java/com/xc/tomcat/bio/http/XCResponse.java
@@ -0,0 +1,9 @@
+package com.xc.tomcat.bio.http;
+
+/**
+ * description
+ *
+ * @author lichao chao.li07@hand-china.com 5/13/21 4:06 PM
+ */
+public class XCResponse {
+}
---
+++ src/main/java/com/xc/tomcat/bio/http/XCServlet.java
@@ -0,0 +1,9 @@
+package com.xc.tomcat.bio.http;
+
+/**
+ * description
+ *
+ * @author lichao chao.li07@hand-china.com 5/13/21 4:07 PM
+ */
+public class XCServlet {
+}
